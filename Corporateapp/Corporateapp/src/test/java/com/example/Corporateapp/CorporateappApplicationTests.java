74efc7818a4a4deed8bf864f2c082f45c4c171a1
Jaishree B <jai.cloud1993@gmail.com>
2025-09-18 14:07:42 +0000

    Initial commit

---
+++ Corporateapp/Corporateapp/src/test/java/com/example/Corporateapp/CorporateappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Corporateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
